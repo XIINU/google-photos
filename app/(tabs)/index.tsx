ee80ab232f56585f7a383594a4c263a16771db04
xiinu <zinu.zinesh47@gmail.com>
2024-09-23 15:11:08 +0530

    Images loaded etc.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,46 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { StyleSheet, View, Text, FlatList, Image } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
+// import { Image } from 'expo-image';
 
 export default function Home() {
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+  const [localAssets, setLocalAssets] = useState<MediaLibrary.Asset[]>([]);
+
+  useEffect(() => {
+    if (permissionResponse?.status !== 'granted') {
+      requestPermission();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (permissionResponse?.status === 'granted') {
+      loadLocalAssets();
+    }
+  }, [permissionResponse]);
+
+  const loadLocalAssets = async () => {
+    const assetPage = await MediaLibrary.getAssetsAsync();
+    // console.log(JSON.stringify(assetPage, null, 2));
+    setLocalAssets(assetPage.assets);
+  };
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Photos' }} />
+      <Stack.Screen options={{ title: 'Google Photos' }} />
+
+      <FlatList
+        data={localAssets}
+        numColumns={2}
+        // columnWrapperStyle={{ gap: 2 }}
+        // contentContainerStyle={{ gap: 2 }}
+        columnWrapperClassName="gap-1"
+        contentContainerClassName="gap-1"
+        renderItem={({ item }) => (
+          <Image source={{ uri: item.uri }} style={{ width: '25%', aspectRatio: 1 }} />
+        )}
+      />
     </>
   );
 }
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
